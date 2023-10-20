da087415a9fedaa90c403ad8aa3c4ef30c356b60
ThessarTS <thessarts@gmail.com>
2023-10-20 23:51:42 +0700

    feat: add handler for get/orders, get/orders/:id, post/orders, patch/orders

--- server/routes/orderRoute.js
+++ server/routes/orderRoute.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 
 const OrderController = require("../controllers/orderController");
+const authentication = require("../middlewares/loginAuthentication");
 
-router.get('/')
-router.get('/:id')
-router.post('/')
-router.patch('/:id')
+router.get('/', authentication, OrderController.findAllOrder)
+router.get('/:id', authentication, OrderController.findOrderById)
+router.post('/', authentication, OrderController.createOrder)
+router.patch('/:id', authentication, OrderController.updateOrderStatus)
 router.delete('/:id')
 router.get('/vehicle/:vehicleid')
 
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -3,10 +3,10 @@ const UserController = require("../controllers/userController");
 const authentication = require("../middlewares/loginAuthentication");
 
 
-router.get("/profiles", authentication, UserController.getProfile);
-router.post("/profiles", authentication, UserController.createProfile);
-router.delete("/profiles", authentication, UserController.deleteUser);
-router.put("/profiles", authentication, UserController.editProfile);
+router.get("/", authentication, UserController.getProfile);
+router.post("/", authentication, UserController.createProfile);
+router.delete("/", authentication, UserController.deleteUser);
+router.put("/", authentication, UserController.editProfile);
 
 
 module.exports = router;
