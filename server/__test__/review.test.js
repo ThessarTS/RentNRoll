c3ebce750ca81ad5fafdc26dd6cdda0f6d5f5500
ahmadrusdiilham <mbem.ilham@gmail.com>
2023-10-21 18:55:07 +0700

    core: order test, review test

--- server/__test__/review.test.js
+++ server/__test__/review.test.js
@@ -18,22 +18,26 @@ let userId = "";
 beforeAll(async () => {
   let dataReview = [
     {
-      rating: '4,4',
+      rating: 4,
       message: "Memuaskan",
-      VehicleId: 1,
-      UserId: 1,
+      VehicleId: 2,
+      UserId: 2,
       createdAt: new Date(),
       updatedAt: new Date(),
     },
     {
-      rating: '3,3',
+      rating: 3,
       message: "Biasa aja",
-      VehicleId: 1,
-      UserId: 1,
+      VehicleId: 3,
+      UserId: 2,
       createdAt: new Date(),
       updatedAt: new Date(),
     },
   ];
+  let dataCategory = require("../data/category.json");
+  dataCategory.forEach((el) => {
+    el.createdAt = el.updatedAt = new Date();
+  });
   let dataVehicle = require("../data/vehicle.json");
   dataVehicle.forEach((el) => {
     el.createdAt = el.updatedAt = new Date();
@@ -43,9 +47,10 @@ beforeAll(async () => {
     el.createdAt = el.updatedAt = new Date();
     el.password = hashPassword(el.password);
   });
-  await sequelize.queryInterface.bulkInsert("Users", dataUser)
-  await sequelize.queryInterface.bulkInsert("Vehicles", dataVehicle)
-  await sequelize.queryInterface.bulkInsert("Reviews", dataReview)
+  await sequelize.queryInterface.bulkInsert("Users", dataUser);
+  await sequelize.queryInterface.bulkInsert("Categories", dataCategory);
+  await sequelize.queryInterface.bulkInsert("Vehicles", dataVehicle);
+  await sequelize.queryInterface.bulkInsert("Reviews", dataReview);
   const userToken = await User.findOne({
     where: {
       email: "pablo@mail.com",
@@ -65,6 +70,11 @@ afterAll(async () => {
     cascade: true,
     restartIdentity: true,
   });
+  await sequelize.queryInterface.bulkDelete("Categories", null, {
+    truncate: true,
+    cascade: true,
+    restartIdentity: true,
+  });
   await sequelize.queryInterface.bulkDelete("Users", null, {
     truncate: true,
     cascade: true,
@@ -74,77 +84,58 @@ afterAll(async () => {
 
 describe("Test get data review endpoint /reviews", () => {
   it("Successfully get review", async function () {
-    const response = (await request(app).get("/reviews")).set(
-      "access_token",
-      access_token
-    );
+    const response = await request(app)
+      .get("/reviews")
+      .set("access_token", access_token);
     expect(response.status).toBe(200);
     expect(response.body).toBeInstanceOf(Array);
   });
-  it("Failed get review without access token", async function () {
+  it("Failed get review", async function () {
     const response = await request(app).get("/reviews");
     expect(response.status).toBe(401);
+    expect(response.body).toBeInstanceOf(Object);
     expect(response.body).toHaveProperty("message", expect.any(String));
   });
 });
 
-describe("Test post review endpoint /reviews", () => {
+describe("Test post review endpoint /reviews/:VehicleId", () => {
   it("Successfully post review", async function () {
     const response = await request(app)
-      .post("/reviews")
+      .post("/reviews/1")
+      .set("access_token", access_token)
       .send({
-        rating: 4,
-        message: "Asik",
-        VehicleId: 10,
-        customerId: 1,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      })
-      .set("access_token", access_token);
+        message: "luar biasa",
+        rating: 5,
+        // UserId: req.user.id,
+        // VehicleId: VehicleId,
+      });
+    expect(response.status).toBe(201);
+    expect(response.body).toBeInstanceOf(Object);
+    expect(response.body).toHaveProperty("message", expect.any(String));
+  });
+  it("Failed post review without message", async function () {
+    const response = await request(app)
+      .post("/reviews/1")
+      .set("access_token", access_token)
+      .send({
+        message: "",
+        rating: 5,
+        // UserId: req.user.id,
+        // VehicleId: VehicleId,
+      });
     expect(response.status).toBe(400);
+    expect(response.body).toBeInstanceOf(Object);
     expect(response.body).toHaveProperty("message", expect.any(String));
   });
   it("Failed post review without rating", async function () {
     const response = await request(app)
-      .post("/reviews")
+      .post("/reviews/1")
+      .set("access_token", access_token)
       .send({
-        message: "Asik",
-        VehicleId: 10,
-        customerId: 1,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      })
-      .set("access_token", access_token);
+        message: "luar biasa",
+      });
     expect(response.status).toBe(400);
-    expect(response.body).toHaveProperty("message", expect.any(String));
-  });
-  it("Failed post review without massage", async function () {
-    const response = await request(app)
-      .post("/reviews")
-      .send({
-        rating: 4,
-        message: "",
-        VehicleId: 10,
-        customerId: 1,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      })
-      .set("access_token", access_token);
-    expect(response.status).toBe(400);
-    expect(response.body).toHaveProperty("message", expect.any(String));
-  });
-  it("Failed post review without VehicleId", async function () {
-    const response = await request(app)
-      .post("/reviews")
-      .send({
-        rating: 4,
-        message: "Asik",
-        customerId: 1,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      })
-      .set("access_token", access_token);
-    expect(response.status).toBe(400);
+    expect(response.body).toBeInstanceOf(Object);
     expect(response.body).toHaveProperty("message", expect.any(String));
   });
 });
